e5cfc45a99a90edccebb89cab1d131b046e1b5e6
AzureWolf <AzureWolf@JPro.home>
2014-10-21 22:44:09 -0400

    Create makeCacheMatrix and cacheSolve functions based on makeVector and cachemean functions

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,34 @@
 ## functions do
 
 ## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## Makes a matrix
+makeCacheMatrix <- function(x = matrix(...)) {
+		m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(solve) m <<- solve
+        getinverse <- function() m
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
 ## Write a short comment describing this function
-
+## Caculates and stores inverse of matrix
+## prevents recalculating if inverse value already exists
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+	## Return a matrix that is the inverse of 'x'
+		m <- x$getinverse()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinverse(m)
+        m
 }
